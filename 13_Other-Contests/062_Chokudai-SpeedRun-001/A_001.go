21c59b627d5080b2e34e75c17d6a0b03fa0a7ddf
solareenlo <solareenlo@gmail.com>
2021-07-15 06:39:26 +0900

    add: Chokudai-SpeedRun-001_a

---
+++ 13_Other-Contests/062_Chokudai-SpeedRun-001/A_001.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+	var n int
+	fmt.Scan(&n)
+	a := make([]int, n)
+	for i := range a {
+		fmt.Scan(&a[i])
+	}
+	sort.Sort(sort.IntSlice(a))
+	fmt.Println(a[len(a)-1])
+}
---
+++ 13_Other-Contests/062_Chokudai-SpeedRun-001/README.md
@@ -0,0 +1,4 @@
+# [Chokudai SpeedRun 001](https://atcoder.jp/contests/chokudai_S001)
+
+## A - 最大値
+- sort
