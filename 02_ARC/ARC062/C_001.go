bf95e90cbcbb2de6e3f2c6075cebff0ae550aad5
solareenlo <solareenlo@gmail.com>
2021-07-22 03:15:20 +0900

    add: ARC062_c

---
+++ 02_ARC/ARC062/C_001.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	var n, a, b int
+	fmt.Scan(&n, &a, &b)
+	for i := 0; i < n-1; i++ {
+		var c, d int
+		fmt.Scan(&c, &d)
+		x := max((a-1)/c+1, (b-1)/d+1)
+		a, b = c*x, d*x
+	}
+	fmt.Println(a + b)
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
