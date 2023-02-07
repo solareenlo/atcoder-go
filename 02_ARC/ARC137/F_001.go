c2210b57c7183b012868a9810610d8d6506dd801
solareenlo <solareenlo@gmail.com>
2023-02-07 22:27:46 +0900

    Fix: ARC137_f

--- 02_ARC/ARC137/F_001.go
+++ 02_ARC/ARC137/F_001.go
@@ -2,175 +2,208 @@ package main
 
 import "fmt"
 
-const N = 1050000
+const N = 200000
+const MD = 998244353
+const L = 19 /* L = ceil(log2((N + 1) * 2)) */
+const N_ = (1 << L)
 
-var (
-	R   = make([]int, N)
-	h   = make([]int, N)
-	G   = make([]int, N)
-	k   int
-	lim int
-	A   = make([]int, N)
-	B   = make([]int, N)
-	W   = make([]int, N)
-	f   = make([]int, N)
-)
-
-func Dft(a []int, lim, o int) {
-	for i := 0; i < lim; i++ {
-		if R[i] > i {
-			a[i], a[R[i]] = a[R[i]], a[i]
-		}
-	}
-	for i := 1; i < lim; i <<= 1 {
-		W[0] = 1
-		W[1] = powMod(3, (mod-1)/i/2)
-		for j := 2; j < i; j++ {
-			W[j] = W[j-1] * W[1] % mod
-		}
-		for j := 0; j < lim; j += (i << 1) {
-			for k := 0; k < i; k++ {
-				t := a[i+j+k] * W[k] % mod
-				a[i+j+k] = (a[j+k] - t + mod) % mod
-				a[j+k] += t
-				a[j+k] %= mod
-			}
-		}
-	}
-	if o != 0 {
-		tmp := a[1:lim]
-		tmp = reverseOrderInt(tmp)
-		for i := 0; i < lim-1; i++ {
-			a[i+1] = tmp[i]
-		}
-		t := powMod(lim, mod-2)
-		for i := 0; i < lim; i++ {
-			a[i] *= t
-			a[i] %= mod
-		}
-	}
-}
-
-func reverseOrderInt(a []int) []int {
-	n := len(a)
-	res := make([]int, n)
-	n = copy(res, a)
-	for i, j := 0, n-1; i < j; i, j = i+1, j-1 {
-		res[i], res[j] = res[j], res[i]
-	}
-	return res
-}
-
-func fen(l, r int) {
-	if l == r {
-		if l < k {
-			if l&1 != 0 {
-				h[l] = 0
-			} else {
-				h[l] = G[l/2]
-			}
-		}
-		return
-	}
-	mid := (l + r) >> 1
-	len1 := mid - l
-	len2 := r - l
-
-	fen(l, mid)
-	if r < k {
-		fen(mid+1, r)
-		return
-	}
-
-	for lim = 1; lim <= r-l; lim <<= 1 {
-
-	}
-	for i := 1; i < lim; i++ {
-		R[i] = (R[i>>1] >> 1) | ((lim >> 1) * (i & 1))
-	}
-	for i := 0; i < len1+1; i++ {
-		A[i] = h[i+l]
-	}
-	for i := len1 + 1; i < lim-len1; i++ {
-		A[i] = 0
-	}
-	for i := 0; i < len2+1; i++ {
-		B[i] = f[i]
-	}
-	for i := len2 + 1; i < lim-len2; i++ {
-		B[i] = 0
-	}
-	Dft(A, lim, 0)
-	Dft(B, lim, 0)
-	for i := 0; i < lim; i++ {
-		A[i] *= B[i]
-		A[i] %= mod
-	}
-	Dft(A, lim, 1)
-
-	for i := mid + 1; i < r+1; i++ {
-		h[i] += A[i-l]
-		h[i] %= mod
-	}
-	fen(mid+1, r)
-}
+var vv, ff, gg [N*2 + 1]int
+var ff_, gg_ [N + 1]int
+var vv_ [L + 1]int
+var wwu, wwv [L + 1][]int
 
 func main() {
-	jc := make([]int, N)
-	inv := make([]int, N)
-	jc[0] = 1
-	jc[1] = 1
-	inv[0] = 1
-	inv[1] = 1
-	G[0] = 1
-	G[1] = 1
-	for i := 2; i < 200011; i++ {
-		jc[i] = jc[i-1] * i % mod
-		inv[i] = inv[mod%i] * (mod - mod/i) % mod
-		G[i] = G[i-1] * (2*i - 1) % mod
-	}
-	for i := 2; i < 200011; i++ {
-		inv[i] *= inv[i-1]
-		inv[i] %= mod
-	}
-
-	var n int
+	aa := make([]int, N+1)
+	bb := make([]int, N+1)
+	cc := make([]int, N+1)
+	dd := make([]int, N+1)
+	ee := make([]int, N+1)
+	Init()
+	var n, k int
 	fmt.Scan(&n, &k)
+	if k == n {
+		fmt.Println(1)
+		return
+	}
 	k++
-
-	for j := 0; j < k/2+1; j++ {
-		var tmp int
-		if j&1 != 0 {
-			tmp = -1
+	for i := 0; i <= n-k; i++ {
+		aa[i] = ((ff[k+i*2] * gg[k] % MD) * gg[i*2] % MD) * ff_[i] % MD
+	}
+	for i := 0; i <= n-k; i++ {
+		cc[i] = (ff[k+i*2] * gg[i*2] % MD) * ff_[i] % MD
+	}
+	for i := 0; i <= k; i++ {
+		dd[i] = ((ff[k] * gg[i] % MD) * gg[k-i] % MD) * gg[i] % MD
+	}
+	for i := 0; i <= n-k; i++ {
+		ee[i] = gg[i*2] * ff_[i] % MD
+	}
+	mult(dd, ee, n+1)
+	for i := 0; i <= n; i++ {
+		if i <= n-k {
+			dd[i] = dd[i] * ff[i*2] % MD
 		} else {
-			tmp = 1
+			dd[i] = 0
 		}
-		f[k-2*j] = tmp * jc[k] * inv[2*j] % mod * inv[k-2*j] % mod * G[j] % mod
 	}
-	for i := 0; i < k; i++ {
-		f[i] = (mod - f[i]) % mod
+	inv(dd, bb, n-k+1)
+	mult(aa, bb, n-k+1)
+	ans := ff_[n]
+	for i := 0; i <= n-k; i++ {
+		ans = (ans - aa[i]*cc[n-k-i]%MD + MD) % MD
 	}
-	for i := 0; i < k/2+1; i++ {
-		f[i], f[k-i] = f[k-i], f[i]
-	}
-	f[0] = 0
-
-	fen(0, 2*n)
-	ans := h[2*n]
-	ans = (ans%mod + mod) % mod
-	fmt.Println(ans * powMod(G[n], mod-2) % mod)
+	ans = ans * gg_[n] % MD
+	fmt.Println(ans)
 }
 
-const mod = 998244353
-
-func powMod(a, n int) int {
-	res := 1
-	for n > 0 {
-		if n%2 == 1 {
-			res = res * a % mod
-		}
-		a = a * a % mod
-		n /= 2
+func inv(aa, bb []int, n int) {
+	if n == 1 {
+		bb[0] = 1
+		return
 	}
-	return res
+	m := (n + 1) / 2
+	aa_ := make([]int, N_)
+	bb_ := make([]int, N_)
+	inv(aa, bb, m)
+	l_ := 0
+	for 1<<l_ < n*2 {
+		l_++
+	}
+	n_ := 1 << l_
+	for i := 0; i < n_; i++ {
+		aa_[i] = 0
+		bb_[i] = 0
+		if i < n {
+			aa_[i] = aa[i]
+		}
+		if i < m {
+			bb_[i] = bb[i]
+		}
+	}
+	ntt(aa_, l_, 0)
+	ntt(bb_, l_, 0)
+	for i := 0; i < n_; i++ {
+		aa_[i] = bb_[i] * (MD + 2 - aa_[i]*bb_[i]%MD) % MD
+	}
+	ntt(aa_, l_, 1)
+	for i := 0; i < n; i++ {
+		bb[i] = aa_[i] * vv_[l_] % MD
+	}
+}
+
+func mult(aa, bb []int, n int) {
+	aa_ := make([]int, N_)
+	bb_ := make([]int, N_)
+	l_ := 0
+	for 1<<l_ < n*2-1 {
+		l_++
+	}
+	n_ := 1 << l_
+	for i := 0; i < n_; i++ {
+		if i < n {
+			aa_[i] = aa[i]
+			bb_[i] = bb[i]
+		} else {
+			aa_[i] = 0
+			bb_[i] = 0
+		}
+	}
+	ntt(aa_, l_, 0)
+	ntt(bb_, l_, 0)
+	for i := 0; i < n_; i++ {
+		aa_[i] = aa_[i] * bb_[i] % MD
+	}
+	ntt(aa_, l_, 1)
+	for i := 0; i < n; i++ {
+		aa[i] = aa_[i] * vv_[l_] % MD
+	}
+}
+
+func ntt(aa []int, l, inverse int) {
+	n := 1 << l
+	for i, j := 0, 1; j < n; j++ {
+		b := n >> 1
+		i ^= b
+		for i < b {
+			b >>= 1
+			i ^= b
+		}
+		if i < j {
+			aa[i], aa[j] = aa[j], aa[i]
+		}
+	}
+	ntt_(aa, l, inverse)
+}
+
+func ntt_(aa []int, l, inverse int) {
+	if l != 0 {
+		n := 1 << l
+		m := n >> 1
+		var ww []int
+		if inverse != 0 {
+			ww = wwv[l]
+		} else {
+			ww = wwu[l]
+		}
+		ntt_(aa, l-1, inverse)
+		tmp := aa[m:]
+		ntt_(tmp, l-1, inverse)
+		for i := 0; i+m < n; i++ {
+			j := i + m
+			a := aa[i]
+			b := aa[j] * ww[i] % MD
+			aa[i] = (a + b) % MD
+			aa[j] = (a - b + MD) % MD
+		}
+	}
+}
+
+func Init() {
+	ff[0] = 1
+	gg[0] = 1
+	for i := 1; i <= N*2; i++ {
+		if i == 1 {
+			vv[i] = 1
+		} else {
+			vv[i] = vv[i-MD%i] * (MD/i + 1) % MD
+		}
+		ff[i] = ff[i-1] * i % MD
+		gg[i] = gg[i-1] * vv[i] % MD
+	}
+	ff_[0] = 1
+	gg_[0] = 1
+	for i := 1; i <= N; i++ {
+		ff_[i] = ff_[i-1] * (i*2 - 1) % MD
+		gg_[i] = gg_[i-1] * vv[i*2-1] % MD
+	}
+	u := power(3, (MD-1)>>L)
+	v := power(u, MD-2)
+	for l := L; l > 0; l-- {
+		n := 1 << l
+		m := n >> 1
+		vv_[l] = power(1<<l, MD-2)
+		wwu[l] = make([]int, m)
+		wwv[l] = make([]int, m)
+		wwu[l][0] = 1
+		wwv[l][0] = 1
+		for i := 1; i < m; i++ {
+			wwu[l][i] = wwu[l][i-1] * u % MD
+			wwv[l][i] = wwv[l][i-1] * v % MD
+		}
+		u = u * u % MD
+		v = v * v % MD
+	}
+	vv_[0] = 1
+}
+
+func power(a, k int) int {
+	p := 1
+	for k != 0 {
+		if (k & 1) != 0 {
+			p = p * a % MD
+		}
+		a = a * a % MD
+		k >>= 1
+	}
+	return p
 }
